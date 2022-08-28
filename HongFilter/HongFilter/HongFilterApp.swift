df2eb86e08e3e254a1fa9c695ce7a1d87dfbc572
otoolz <otool.hf@gmail.com>
2022-08-28 18:10:11 +0900

    프로젝트 생성 🚀
    프로젝트 생성 🚀

---
+++ HongFilter/HongFilter/HongFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HongFilterApp.swift
+//  HongFilter
+//
+//  Created by KiWoong Hong on 2022/08/28.
+//
+
+import SwiftUI
+
+@main
+struct HongFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
