8a4777766ec30ecbcf5e02abce0c6010031a1e90
otoolz <otool.hf@gmail.com>
2022-08-29 11:55:39 +0900

    [Add] Models/HongFilterModel 추가
    Models/HongFilterModel 추가

---
+++ HongFilter/HongFilter/Models/HongFilterModel.swift
@@ -0,0 +1,31 @@
+//
+//  HongFilterModel.swift
+//  HongFilter
+//
+//  Created by KiWoong Hong on 2022/08/29.
+//
+
+import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
+
+enum HongFilter: CaseIterable {
+   /*
+    case gaussianBlur
+    case bloom
+    
+    var name: String {
+        switch self {
+        case .gaussianBlur : return "gaussianBlur"
+        case .bloom : return "bloom"
+        }
+    }
+    
+    var filter: CIFilter {
+        switch self {
+        case .bloom : return CIFilter.bloom()
+        case .gaussianBlur : return CIFilter.gaussianBlur()
+        }
+    }
+    */
+}
