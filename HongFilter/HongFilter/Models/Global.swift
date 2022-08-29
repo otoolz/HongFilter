c3c2e5f9e196a18fe921cc11555f4f7ce49b5a0d
otoolz <otool.hf@gmail.com>
2022-08-29 17:25:21 +0900

    [Feat] Global 모델 추가
    Global 모델 추가

---
+++ HongFilter/HongFilter/Models/Global.swift
@@ -0,0 +1,18 @@
+//
+//  Global.swift
+//  HongFilter
+//
+//  Created by KiWoong Hong on 2022/08/29.
+//
+
+import SwiftUI
+
+enum Global {
+    static var screenWidth: CGFloat {
+        UIScreen.main.bounds.size.width
+    }
+    
+    static var screenHeight: CGFloat {
+        UIScreen.main.bounds.size.height
+    }
+}
